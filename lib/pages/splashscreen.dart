9dc1ee80ff04b4dfe28c75966616ea555fbbb9b2
Diperson Maharjan <sdiperson20@gmail.com>
2024-05-24 17:19:09 +0545

    Splash Screen and Images

---
+++ lib/pages/splashscreen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:gharbhada/pages/home_page.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    //_navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(Duration(milliseconds: 1500), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => HomePage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              child: Image.asset('assets/images/Logo-gharbhada.png'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
