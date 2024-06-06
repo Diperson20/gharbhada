5e15aebd31a0bcb5783a11f7ebde32213066c026
Diperson Maharjan <sdiperson20@gmail.com>
2024-06-06 15:23:27 +0545

    updated login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -50,7 +51,7 @@ class _LoginState extends State<Login> {
               ),
             ),
             const SizedBox(
-              height: 20,
+              height: 10,
             ),
             Expanded(
               // Login below code
@@ -63,91 +64,87 @@ class _LoginState extends State<Login> {
                 ),
                 child: Padding(
                   padding: EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 60,
-                      ),
-                      Container(
-                        //container login and password info
-                        padding: EdgeInsets.all(30),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color.fromRGBO(225, 95, 27, 3),
-                                blurRadius: 20,
-                                offset: Offset(0, 10),
-                              )
-                            ]),
-                        child: Column(
-                          //Input box
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          //container login and password info
+                          padding: EdgeInsets.all(30),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: const Color.fromARGB(255, 33, 40, 243)
+                                      .withOpacity(0.5),
+                                  blurRadius: 10,
+                                  offset: Offset(0, 5),
+                                )
+                              ]),
+                          child: Column(
+                            //Input box
+                            children: [
+                              Container(
+                                // Input for email
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                        color: const Color.fromARGB(
+                                            255, 11, 10, 10)!),
+                                  ),
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Email or Phone number',
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                // input for password
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                        color:
+                                            Color.fromARGB(255, 100, 89, 89)!),
+                                  ),
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Password',
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ), //this is a pass login container
+                        SizedBox(
+                            height: MediaQuery.of(context).size.width * 0.07),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
                           children: [
                             Container(
-                              // Input for email
-                              decoration: BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(
-                                      color: const Color.fromARGB(
-                                          255, 11, 10, 10)!),
-                                ),
-                              ),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  hintText: 'Email or Phone number',
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  border: InputBorder.none,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              // input for password
-                              decoration: BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(
-                                      color: const Color.fromARGB(
-                                          255, 17, 14, 14)!),
-                                ),
-                              ),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  hintText: 'Password',
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  border: InputBorder.none,
-                                ),
+                              child: Text(
+                                "Forget Password?",
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 62, 60, 185),
+                                    fontWeight: FontWeight.bold),
                               ),
                             ),
                           ],
                         ),
-                      ), //this is a pass login container
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 20),
-                                child: Text(
-                                  "Forget Password?",
-                                  style: TextStyle(
-                                      color: Colors.grey,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 20, left: 20),
-                        child: Row(
+
+                        SizedBox(
+                            height: MediaQuery.of(context).size.width * 0.07),
+
+                        Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Container(
@@ -156,7 +153,7 @@ class _LoginState extends State<Login> {
                               width: 150,
                               decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(50),
-                                color: Colors.orange,
+                                color: Color.fromARGB(255, 62, 60, 185),
                               ),
                               child: Center(
                                 child: Text(
@@ -174,7 +171,7 @@ class _LoginState extends State<Login> {
                               width: 150,
                               decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(50),
-                                color: Colors.orange,
+                                color: Color.fromARGB(255, 62, 60, 185),
                               ),
                               child: Center(
                                 child: Text(
@@ -188,24 +185,80 @@ class _LoginState extends State<Login> {
                             ),
                           ],
                         ),
-                      ),
-                      Container(
-                        child: Text("Sign in with"),
-                      ),
-                      Container(
-                        child: Row(
-                          children: [
-                            Container(
-                              height: 50,
-                              width: 50,
-                              decoration: BoxDecoration(
-                                  color: Colors.blue,
-                                  borderRadius: BorderRadius.circular(100)),
-                            ), //google
-                          ],
+                        SizedBox(
+                            height: MediaQuery.of(context).size.width * 0.05),
+
+                        Container(
+                          child: Text("Sign in with"),
                         ),
-                      )
-                    ],
+                        SizedBox(
+                            height: MediaQuery.of(context).size.width * 0.05),
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: MediaQuery.of(context).size.width *
+                                    0.1, // Responsive height (10% of screen width)
+                                width: MediaQuery.of(context).size.width *
+                                    0.1, // Responsive width (10% of screen width)
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(100),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black
+                                          .withOpacity(0.3), // Shadow color
+                                      spreadRadius: 1,
+                                      blurRadius: 5,
+                                      offset: Offset(
+                                          0, 3), // Shadow position (top-left)
+                                    ),
+                                  ],
+                                ),
+                                child: FaIcon(
+                                  FontAwesomeIcons.google,
+                                  size: MediaQuery.of(context).size.width *
+                                      0.04, // Responsive icon size (4% of screen width)
+                                  color: Colors.white, // Icon color
+                                ),
+                              ),
+                              SizedBox(
+                                  width: MediaQuery.of(context).size.width *
+                                      0.05), // Responsive space between containers (5% of screen width)
+                              Container(
+                                height: MediaQuery.of(context).size.width *
+                                    0.1, // Responsive height (10% of screen width)
+                                width: MediaQuery.of(context).size.width *
+                                    0.1, // Responsive width (10% of screen width)
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(100),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black
+                                          .withOpacity(0.3), // Shadow color
+                                      spreadRadius: 1,
+                                      blurRadius: 5,
+                                      offset: Offset(
+                                          0, 3), // Shadow position (top-left)
+                                    ),
+                                  ],
+                                ),
+                                child: FaIcon(
+                                  FontAwesomeIcons.facebook,
+                                  size: MediaQuery.of(context).size.width *
+                                      0.04, // Responsive icon size (4% of screen width)
+                                  color: Colors.white, // Icon color
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
